c72a48a0a8bd70b79636f1406bb28a1ef7e0dc78
Evgeny Yakuta <yakutawork@gmail.com>
2023-06-09 19:25:56 +0400

    fix 8 out of 9 comments

--- cypress/pages/gamePage.js
+++ cypress/pages/gamePage.js
@@ -1,24 +1,21 @@
-class gamePage{
-    elements = {
-        searchTag : () => cy.xpath('//*[@id="searchtag_tmpl"]//span'),
-        gameResulsRow : () => cy.xpath('//*[@id="search_resultsRows"]//a'),
-        sortGameMenu : () => cy.xpath('//*[@id="sort_by_dselect_container"]'),
-        descElement : () => cy.xpath('//*[@id="Price_DESC"]')
-    }
+class gamePage {
+	elements = {
+		searchTag: () => cy.xpath('//*[@id="searchtag_tmpl"]//span'),
+		gameResulsRow: () => cy.xpath('//*[@id="search_resultsRows"]//a'),
+		sortGameMenu: () => cy.xpath('//*[@id="sort_by_dselect_container"]'),
+		descElement: () => cy.xpath('//*[@id="Price_DESC"]'),
+        prices: () => cy.get('div[class="col search_price_discount_combined responsive_secondrow"]')
+	};
 
-    checkPageIsOpen = function(text) {
-        this.elements.searchTag().should('contain', text)
-    }
+	sortGameByDesc = function () {
+		this.elements.sortGameMenu().click();
+		this.elements.descElement().click();
+		cy.wait(2000);
+	};
 
-    checkGameRowIsNotEmpty = function() {
-        this.elements.gameResulsRow().should('exist')
-    }
-
-    sortGameByDesc = function() {
-        this.elements.sortGameMenu().click()
-        this.elements.descElement().click()
-        cy.wait(2000)
-    }
+    getPrices =  (count) => {
+        return this.elements.prices().filter(`:lt(${count})`).mapInvoke('getAttribute','data-price-final');
+    };
 }
 
 module.exports = new gamePage();
