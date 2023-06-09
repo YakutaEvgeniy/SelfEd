427af918f89a6ad739597dd164238e707f815193
Evgeny Yakuta <yakutawork@gmail.com>
2023-06-09 19:30:31 +0400

    update code style

--- cypress/e2e/Qa/steamApp.cy.js
+++ cypress/e2e/Qa/steamApp.cy.js
@@ -1,30 +1,28 @@
-import homePage from "../../pages/homePage"
-import gamePage from "../../pages/gamePage"
-import "cypress-map"
+import homePage from "../../pages/homePage";
+import gamePage from "../../pages/gamePage";
+import "cypress-map";
 
-const testData = require('../../fixtures/test-data.json');
-const env = require('../../fixtures/envConfig.json');
+const testData = require("../../fixtures/test-data.json");
+const env = require("../../fixtures/envConfig.json");
 
-describe('Steam test', () => {
-  testData.forEach((data) => {
-    it('check steam', () => {
-      cy.visit(env.url,{
-        headers:{
-          'Accept-Language': env.language
-        }
-      }); 
-     
-      homePage.elements.homePageContent().should("be.visible")
-      homePage.findGame(data.game);
-      gamePage.elements.searchTag().should("contain", data.game);
-      gamePage.elements.gameResulsRow().should("exist");
-      gamePage.sortGameByDesc();   
+describe("Steam test", () => {
+	testData.forEach((data) => {
+		it("check steam", () => {
+			cy.visit(env.url, {
+				headers: {
+					"Accept-Language": env.language,
+				},
+			});
 
-      gamePage.getPrices(data.count).then((list)=>{
-        expect(list).to.be.sorted({descending: true});
-      })
-    })
-  })  
-})
+			homePage.elements.homePageContent().should("be.visible");
+			homePage.findGame(data.game);
+			gamePage.elements.searchTag().should("contain", data.game);
+			gamePage.elements.gameResulsRow().should("exist");
+			gamePage.sortGameByDesc();
 
-  
+			gamePage.getPrices(data.count).then((list) => {
+				expect(list).to.be.sorted({ descending: true });
+			});
+		});
+	});
+});
--- cypress/pages/gamePage.js
+++ cypress/pages/gamePage.js
@@ -4,7 +4,10 @@ class gamePage {
 		gameResulsRow: () => cy.xpath('//*[@id="search_resultsRows"]//a'),
 		sortGameMenu: () => cy.xpath('//*[@id="sort_by_dselect_container"]'),
 		descElement: () => cy.xpath('//*[@id="Price_DESC"]'),
-        prices: () => cy.get('div[class="col search_price_discount_combined responsive_secondrow"]')
+		prices: () =>
+			cy.get(
+				'div[class="col search_price_discount_combined responsive_secondrow"]',
+			),
 	};
 
 	sortGameByDesc = function () {
@@ -13,9 +16,12 @@ class gamePage {
 		cy.wait(2000);
 	};
 
-    getPrices =  (count) => {
-        return this.elements.prices().filter(`:lt(${count})`).mapInvoke('getAttribute','data-price-final');
-    };
+	getPrices = (count) => {
+		return this.elements
+			.prices()
+			.filter(`:lt(${count})`)
+			.mapInvoke("getAttribute", "data-price-final");
+	};
 }
 
 module.exports = new gamePage();
